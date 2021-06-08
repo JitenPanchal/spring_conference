a7cddd7db04e69ff563be12a9c7b43f5c566c661
jpanchal <jiten.panchal@icx4..com>
2021-06-08 19:33:42 +0100

    Add all

--- conference/src/main/java/com/pluralsight/conference/controllers/ApiController.java
+++ conference/src/main/java/com/pluralsight/conference/controllers/ApiController.java
@@ -10,12 +10,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pluralsight.conference.entities.UserDetails;
+import com.pluralsight.conference.exceptions.EntityNotFoundException;
 import com.pluralsight.conference.model.Address;
 import com.pluralsight.conference.model.Info;
 import com.pluralsight.conference.model.UserDetailsRequestModel;
@@ -50,12 +52,24 @@ public class ApiController {
 		res.getWriter().println("response from infoFromServlet");
 	}
 
-	@PostMapping(value = "/user", produces = { "application/json", "application/xml" })
+	@PostMapping(value = "/saveuser", produces = { "application/json", "application/xml" })
 	private ResponseEntity<UserDetails> saveUser(@Valid @RequestBody UserDetailsRequestModel model) {
 
 		var ud = modelMapper.map(model, UserDetails.class);
 
 		return new ResponseEntity<UserDetails>(ud, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/user/{id}/{id2}", produces = { "application/json", "application/xml" })
+	private ResponseEntity<UserDetails> getUser(@PathVariable("id") int id2, @PathVariable("id2") int id) {
+		return new ResponseEntity<UserDetails>(getUserFromService(id), HttpStatus.OK);
+	}
+
+	private static UserDetails getUserFromService(int id) {
+		if (id <= 0)
+			throw new EntityNotFoundException();
+
+		return new UserDetails();
 	}
 
 }
